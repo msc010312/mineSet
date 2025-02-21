b6a332e7286ef37d27a8a5bf1fbb34a45bf696e7
matic <msc0101312@gmail.com>
2025-02-21 14:13:58 +0900

    2025-02-21

--- js/control.js
+++ js/control.js
@@ -1,3 +1,4 @@
+// 지뢰생성버튼
 const addbtn = document.querySelector('.add');
 const conJoin = document.conJoin;
 const row = conJoin.row;
@@ -21,11 +22,15 @@ addbtn.addEventListener('click', (e) => {
     row.value = '';
     col.value = '';
     mine.value = '';
-})
+});
+
+
 
 function newmap(rowCol) {
     // const row = document.getElementById('row').value;
     // const col = document.getElementById('col').value;
+
+    // 테이블 생성
     const tbl = document.getElementById('map');
 
     while (tbl.firstChild) {
@@ -39,16 +44,40 @@ function newmap(rowCol) {
         for (let i = 0; i < rowCol.row; i++) {
             const newtr = document.createElement('tr')
             for (let j = 0; j < rowCol.col; j++) {
-
-                const newtd = document.createElement('td')
-                newtr.appendChild(newtd)
+                const newtd = document.createElement('td');
+                newtd.classList.add('normal')
+                newtd.addEventListener('click', (e) => {
+                    if(e.target.classList.contains('flag')){
+                        return;
+                    }
+                    if (e.target.classList.contains('mine')) {
+                        gameOver();
+                    }
+                     else {
+                        e.target.classList.add('open');
+                        e.target.classList.remove('normal');
+                        e.target.style.setProperty('border-color','#8d0d26',"important")
+                    }
+                });
+                newtd.addEventListener('contextmenu',(e)=>{
+                    e.preventDefault();
+                    e.target.classList.toggle('flag');
+                })
+                newtr.appendChild(newtd);
             }
-            tbl.appendChild(newtr)
+            tbl.appendChild(newtr);
         }
     }
+    // const calltd = document.querySelector('#map>tr>td')
+    // calltd.addEventListener('click', (e) => {
+    //     if (e.target.tagName === 'td' && e.target.classList.contains('normal')) {
+    //         e.target.classList.add('open')
+    //     }
+    // });
 };
 
 
+// 지뢰 생성
 function setMine(mineNumArr) {
     let minesarr = new Set();
     while (minesarr.size < mineNumArr.mineNum) {
@@ -60,6 +89,7 @@ function setMine(mineNumArr) {
     return Array.from(minesarr);
 }
 
+// 지뢰 삽입
 function pushMine(minesarr) {
     const tdArr = document.getElementsByTagName('td');
     for (let i = 0; i < tdArr.length; i++) {
